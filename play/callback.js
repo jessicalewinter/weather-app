d501b14635f5e102cfb2f963dedc9e8a989765ed
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-24 19:28:51 -0300

    Adding the weather-app repository

---
+++ play/callback.js
@@ -0,0 +1,12 @@
+var getUser = (id, callback) => {
+	  var user = {
+		      id: id,
+		          name: 'Bia',
+			    };
+	    callback(user);
+};
+
+getUser(31, (userObj) =>{
+	  console.log(userObj);
+});
+
