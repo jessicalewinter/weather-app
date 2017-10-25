1be2a3f697219e5e841ff3004a1548a752052164
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-25 15:48:40 -0300

    Printing latitude and longitude of Central Park-NY from google maps api

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request =  require('request');
 //reqeuest
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20Rio%20de%20Janeiro',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Central%20Park%20New%20York&key=AIzaSyBjMzXbmGTwhTPT5zB1ytzilPZNjguTpzg',
   json: true,
 }, (error, response, body) => {
-  console.log(JSON.stringify(body,undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 });
