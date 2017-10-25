49e50b053c5207614c5f519c64c323383a2dcc72
Jessica Lewinter <jessica@DESKTOP-P5ABIVV.localdomain>
2017-10-25 10:45:16 -0300

    Stringify the json request of maps api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request =  require('request');
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Fortaleza',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20Rio%20de%20Janeiro',
   json: true,
-}, (error, response,body) => {
-  console.log(body);
+}, (error, response, body) => {
+  console.log(JSON.stringify(body,undefined, 2));
 });
