18ca2b8d792d325420c3ca84aa93ccf045a89d86
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-25 19:46:14 -0300

    Adding geocode folder and refactor the code to show error messages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request =  require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs.
   options({
@@ -14,18 +15,11 @@ const argv = yargs.
   .alias('help', 'h')
   .argv;
 
-var addressAPI = encodeURIComponent(argv.address);
-console.log(addressAPI);
-
-
-//request
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressAPI}`,
-  json: true,
-}, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if(errorMessage){
+    console.log(errorMessage);
+  }else{
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
-
 
