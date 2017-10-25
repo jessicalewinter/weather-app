ac4359f024f6ab9a7cc74670f7c4aba1694c0251
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-25 17:35:55 -0300

    Making the input for the weather app dinamically

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const request =  require('request');
-//reqeuest
+const yargs = require('yargs');
+
+const argv = yargs.
+  options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for',
+      string: true,
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var addressAPI = encodeURIComponent(argv.address);
+console.log(addressAPI);
+
+
+//request
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Central%20Park%20New%20York&key=AIzaSyBjMzXbmGTwhTPT5zB1ytzilPZNjguTpzg',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressAPI}`,
   json: true,
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 });
+
+
