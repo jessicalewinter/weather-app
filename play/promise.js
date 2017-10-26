9f1e301772427bdc350da9e16057437b10200a00
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-26 19:52:53 -0300

    Play removed from github

---
+++ play/promise.js
@@ -0,0 +1,10 @@
+var somePromise = new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+        resolve('Hey. It worked!');
+    },2500);
+});
+
+somePromise.then((message)=> {
+    console.log('Success: ', message);
+});
