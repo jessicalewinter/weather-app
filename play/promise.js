2aff5ec196377d8dab5ebfb9994af9c723746c8a
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-26 21:29:58 -0300

    Testing promise function in plays files

--- play/promise.js
+++ play/promise.js
@@ -1,10 +1,35 @@
-var somePromise = new Promise((resolve, reject) => {
-    
-    setTimeout(() => {
-        resolve('Hey. It worked!');
-    },2500);
+var asyncAdd = (num1, num2) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() =>{
+                if(typeof num1 === 'number' && typeof num2 === 'number'){
+                    resolve(num1 + num2);
+                }else{
+                    reject('Arguments are not numbers');
+                }
+            }, 1500);
+    });
+};
+
+asyncAdd(5,'7').then((result) => {
+    console.log('Result:', result);
+    return asyncAdd(result, 33);
+}).then((result) => {
+    console.log('Should be 45?', result);
+}).catch((errorMessage) => {
+    console.log(errorMessage);
 });
 
-somePromise.then((message)=> {
-    console.log('Success: ', message);
-});
+
+// var somePromise = new Promise((resolve, reject) => {
+    
+//     setTimeout(() => {
+//         resolve('Hey. It worked!');
+//         //reject('Unable to fulfill promise');
+//     },2500);
+// });
+
+// somePromise.then((message)=> {
+//     console.log('Success: ', message);
+// },(errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
