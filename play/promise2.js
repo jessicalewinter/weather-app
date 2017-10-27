cacf6b16fb1a6fdc3c060e40ad96248b8cf7b707
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-27 13:48:15 -0300

    Promise2.js fixed

--- play/promise2.js
+++ play/promise2.js
@@ -1,33 +1,29 @@
 const request = require('request');
 
-var geocodeAddress = (location) => {
+var geocodeAddress = (address) => {
     return new Promise((resolve, reject) => {
-        if(typeof location === 'number'){
             var addressAPI = encodeURIComponent(address);
             request({
                 url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressAPI}`,
                 json: true,
                 }, (error, response, body) => {
                 if(error){  
-                    callback('Unable to connect to Google servers.'); 
+                    reject('Unable to connect to Google servers.'); 
                 }else if(body.status === "ZERO_RESULTS"){
-                    callback('Unable to find that address.');
+                    reject('Unable to find that address.');
                 }else if(body.status === "OK"){
-                    callback(undefined, {
+                    resolve({
                         address: body.results[0].formatted_address,
                         latitude: body.results[0].geometry.location.lat,
                         longitude: body.results[0].geometry.location.lng,
                         });
                     }
                 });
-            }else{
-                console.log(reject);
-            }
         });
     };
 
-geocodeAddress('191475').then((location) => {
+geocodeAddress('000000').then((location) => {
     console.log(JSON.stringify(location, undefined, 2));
-}, (errorMessage) =>{
+}, (errorMessage) => {
     console.log(errorMessage);
 });
