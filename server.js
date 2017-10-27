796fc86ff44f6d72cc3185aa8ba7e47be302165d
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-27 18:20:58 -0300

    Using handlebars.js and express requests

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
 //res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Jessica',
-        likes:[
-            'Sex',
-            'Drugs',
-            'Alcohool',
-        ]
+    res.render('home.hbs',{
+        pageTitle: 'Home Title',
+        currentYear: new Date().getFullYear(),
+        welcomeTitle: 'Welcome to this webpage',
     });
 });
 
 app.get('/about',(req, res) => {
-    res.send('About Page');
+    res.render('about.hbs',{
+        pageTitle: 'About Title',
+        currentYear: new Date().getFullYear(),
+    });
 });
 
 app.get('/bad', (req, res) => {
