bf8edb1a1f1e6a49500d1ae2c521768f1995c89c
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-30 15:52:48 -0300

    44-Adding partials to the server

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
 //res.send('<h1>Hello Express!</h1>');
     res.render('home.hbs',{
         pageTitle: 'Home Title',
-        currentYear: new Date().getFullYear(),
         welcomeTitle: 'Welcome to this webpage',
     });
 });
@@ -19,7 +26,6 @@ app.get('/', (req, res) => {
 app.get('/about',(req, res) => {
     res.render('about.hbs',{
         pageTitle: 'About Title',
-        currentYear: new Date().getFullYear(),
     });
 });
 
