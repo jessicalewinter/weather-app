95b0985ee71a1c4173c2315404511a23a4f1956f
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-31 20:03:30 -0300

    Tesing mocha with jest and expect

--- utils/utils.js
+++ utils/utils.js
@@ -4,6 +4,12 @@ module.exports = {
     },
     square: (x) => {
         return x * x;
+    },
+    setName: (user, fullName) => {
+        var names = fullName.split(' ');
+        user.firstName = names[0];
+        user.lastName = names[1];
+        return user;
     }
 }
 
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,37 @@
+const expect =  require('expect');
+
 const utils = require('./utils');
 
 it('Shoulds add two numbers', () => {
     var res = utils.add(33,11);
     
-    if(res !== 44){
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44);
+    
 });
 
 it('Should show the square of a number', () => {
     var res = utils.square(5);
 
-    if(res !== 25){
-        throw new Error(`Expected 25, but got ${res}`);
-    }
+    expect(res).toBe(25);
+
 });
+
+it('Should verify first and last names ', () => {
+    var user = {location:'Brazil', age: 23};
+    var res = utils.setName( user,'Jessica Lewinter');
+
+    expect(res).toMatchObject({
+        firstName: 'Jessica',
+        lastName: 'Lewinter'
+    })
+})
+
+// it('Should expect some values', () => {
+//     // expect({name: 'Jessica'}).not.toEqual({name: 'Jessica'});
+//    expect({
+//        name: 'Jessica',
+//        age:23,
+//        location: 'Brazil',
+//    })
+
+// });
